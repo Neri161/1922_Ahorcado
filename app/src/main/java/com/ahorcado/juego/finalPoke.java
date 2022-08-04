43018bd1e51eb5451f6925d0e0cc5a181dab7418
Neri161 <Neri010601>
2022-08-04 01:21:57 -0500

    Mejoras papu

---
+++ app/src/main/java/com/ahorcado/juego/finalPoke.java
@@ -0,0 +1,14 @@
+package com.ahorcado.juego;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class finalPoke extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_final_poke);
+    }
+}
